ccaa44952ceb18756816224be3a2d3b593afd828
kan3103 <khang310304@gmail.com>
2024-09-01 15:01:03 +0700

    update Signin with google and Blog

--- backend/auths/serializers.py
+++ backend/auths/serializers.py
@@ -3,3 +3,8 @@ from rest_framework import serializers
 class TokenSerializer(serializers.Serializer):
     refresh = serializers.CharField()
     access = serializers.CharField()
+
+class UserSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
