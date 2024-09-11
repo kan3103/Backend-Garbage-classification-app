7f6b30eaf073a54b42f7e16688e453b8dd18751d
kan3103 <khang310304@gmail.com>
2024-09-11 14:26:33 +0700

    Update profile models

--- backend/profiles/urls.py
+++ backend/profiles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import UserListCreateView
+from .views import UserListCreateView,ProfileDetailView
 
 urlpatterns = [
-    path('', UserListCreateView.as_view())
+    path('', UserListCreateView.as_view()),
+    path('profile/<str:id>/',ProfileDetailView.as_view()),
 ]
